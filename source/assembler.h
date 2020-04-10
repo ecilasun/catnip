b98f9b142123ed373fabc60e38e08213e63147b9
Engin Cilasun <ecilasun@me.com>
2020-04-10 18:37:43 -0400

    Linux build fixes

--- source/assembler.h
+++ source/assembler.h
@@ -1,3 +1,8 @@
 
 #include <vector>
+#if defined(CAT_LINUX)
+#include "string.h"
+#include "stdio.h"
+#endif
+#include "inttypes.h"
 int compile_asm(const char *_inputname, const char *_outputname);
