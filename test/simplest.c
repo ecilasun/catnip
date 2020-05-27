27158bb282aaa6e10bae47c93e461b5b8d7687cb
Engin Cilasun <ecilasun@me.com>
2020-05-27 13:16:53 -0400

    Typo fix

--- test/simplest.c
+++ test/simplest.c
@@ -1,3 +1,3 @@
-find c,s:		{ return *s ? *s==c ? 1 : find(c, s+1) : 0; }
+Find c,s:		{ return *s ? *s==c ? 1 : find(c, s+1) : 0; }
 Length s:		{ var result=0; while(*s++) {++result;} return result; }
 LastCharN s,n:	return s[Length(s)-n];
