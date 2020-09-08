9b42cb01e2af8a54955edd8b1dd4976809d7223d
Engin Cilasun <ecilasun@me.com>
2020-09-08 06:04:37 -0400

    Sample cleanup

--- test/simplest.c
+++ test/simplest.c
@@ -1,13 +1,15 @@
-// Test init declaration: A=12, B=-3
+// Test init declaratiors
 int A=2+3*4-8%3, B=3-A/2;
-int C[2*4];
-int D[4] = {10,20,30,40};
-int *VRAM = 0x10000; // NOTE: Actual hardware VRAM starts at 0x80000000, here we cheat for simpler memory access
+int C[2*4]; // Should contain 2*4 DWORDs of 0xCCCCCCCC
+int D[4] = {10, 20, 30, 40};
+
+// NOTE: Actual hardware VRAM starts at 0x80000000, here we cheat for simpler memory access
+int *VRAM = 0x10000;
 
 int test()
 {
      int E = A*B/-6;
      C[2] = 5+D[3];
      VRAM[4] = 0xFF-C[2];     // Should receive 210 at 0x00010004
-     E = &B;
+     E = &B;                  // E should contain address 0x00000001
 }
