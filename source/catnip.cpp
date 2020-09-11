8e1086f5220e5f039a0b615f95615987a72ec00b
Engin Cilasun <ecilasun@me.com>
2020-09-11 00:55:38 -0400

    GrimR

--- source/catnip.cpp
+++ source/catnip.cpp
@@ -7,18 +7,17 @@ int main(int _argc, char **_argv)
 		printf("catnip v0.2\n");
 		printf("(C)2020 Engin Cilasun\n");
 		printf("Usage:\n");
-		printf("{WORK IN PROGRESS!} catnip inputfile.c outputfile.asm - Generates assembly code from input C-like file\n");
+		printf("catnip inputfile.grm outputfile.asm - Generates assembly listing from GrimR code\n");
 		printf("catnip inputfile.asm outputfile.mif - Generates a memory initialization file from assembly input for FPGA device\n");
 		printf("catnip inputfile.asm outputfile.rom - Generates a ROM file from assembly input for emulator\n");
 		printf("catnip inputfile.rom - Runs the emulator with given ROM file\n");
 		return 0;
 	}
 
-	if (strstr(_argv[1], ".c"))
-		return CompileCode(_argv[1], _argv[2]);	   // .C -> .ASM
-		//return CompileCode2(_argv[1], _argv[2]);	   // .C -> .ASM
+	if (strstr(_argv[1], ".grm"))
+		return CompileCode(_argv[1], _argv[2]);		// .GrimR -> .ASM
 	else if (strstr(_argv[1], ".asm"))
-		return compile_asm(_argv[1], _argv[2]);	 // .ASM -> .ROM/.MIF
+		return compile_asm(_argv[1], _argv[2]);		// .ASM -> .ROM/.MIF
 	else
-		return emulate_rom(_argv[1]);			   // .ROM -> Emulator
+		return emulate_rom(_argv[1]);				// .ROM -> Emulator
 }
--- source/compiler.cpp
+++ source/compiler.cpp
@@ -1,38 +1,14 @@
 #include "compiler.h"
 
-//#define OLD_COMPILER_ONE
-//#define OLD_COMPILER_TWO
-
-#if defined(OLD_COMPILER_ONE)
-#include "../grammar/nanoc.y.hpp"
-#elif defined(OLD_COMPILER_TWO)
-#include "../grammar/ec.y.hpp"
-extern void dumpnodes(void);
-#else
-// GrimR - Person Wearing A Mask / Helmet / The Fierce One
+// GrimR : Person Wearing A Mask / Helmet / The Fierce One
 #include "../grammar/grimr.y.hpp"
 extern void dumpnodes(void);
-#endif
 
 int CompileCode(char *_inputname, char * /*_outputname*/)
 {
 	// Test Flex/Bison code
 	extern FILE *yyin;
 	yyin = fopen(_inputname, "r");
-
-#if defined(OLD_COMPILER_ONE)
-	// Prologue
-	printf("// Prologue\n");
-	printf("RESETSTACKCURSOR\n  // Set stack cursor to zero\n");
-	printf("CALL @__global_init\n");
-	printf("CALL @main\n");
-	printf("BREAK\n\n");
-
-	// Global initalizer function
-	printf("// Global initialization\n");
-	printf("@FUNCTION\n");
-	printf("@NAME '__global_init'\n");
-#endif
 
 	int result = yyparse();
 	fclose(yyin);
