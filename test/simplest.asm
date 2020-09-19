f193825041641acaff772a58836dd2d6d3e17bb2
Engin Cilasun <ecilasun@me.com>
2020-09-19 19:12:45 -0400

    FIx for array on RHS, array index expressions now work

--- test/simplest.asm
+++ test/simplest.asm
@@ -4,94 +4,14 @@
 GrimR (c)2020 Engin Cilasun
 ---------------------------
 
-@label DrawRect
-pop    height
-pop    width
-pop    posY
-pop    posX
-ld     r0, [posY]
-st     [spanY], r0
- 
-@label while2
-ld     r0, [posY]
-ld     r1, [height]
-add    r0, r0, r1
-ld     r1, [spanY]
-ld     r1, r1
-cmp.l  r0, r1
-jmp.nz endwhile3
-ld     r0, [posX]
-st     [spanX], r0
- 
-@label while0
-ld     r0, [posX]
-ld     r1, [width]
-add    r0, r0, r1
-ld     r1, [spanX]
-ld     r1, r1
-cmp.l  r0, r1
-jmp.nz endwhile1
-ld     r0, 0x00000140
-ld     r1, [spanY]
-mul    r0, r0, r1
-ld     r1, [spanX]
-ld     r1, r1
-add    r0, r0, r1
-ld     r0, r0
-st     [address], r0
-ld     r0, 0x000000ff
-st     [VRAM+address], r0
-ld     r0, [spanX]
-ld     r1, 0x00000001
-add    r0, r0, r1
-ld     r0, r0
-st     [spanX], r0
-jmp    while0
- 
-@label endwhile1
-ld     r0, [spanY]
-ld     r1, 0x00000001
-add    r0, r0, r1
-ld     r0, r0
-st     [spanY], r0
-jmp    while2
- 
-@label endwhile3
-ret    
 @label main
 ld     r0, 0x80000000
 st     [VRAM], r0
-ld     r0, 0x00000000
-st     [i], r0
- 
-@label while4
-ld     r0, [i]
-ld     r1, 0x0000000a
-cmp.l  r0, r1
-jmp.nz endwhile5
-ld     r0, [i]
-st     [banana+i], r0
-jmp    while4
- 
-@label endwhile5
-ld     r0, 0x00000200
-push   r0
-ld     r0, 0x00000180
-push   r0
-ld     r0, 0x00000080
-push   r0
-ld     r0, 0x00000060
-push   r0
-call   DrawRect
-ld     r0, 0x00000001
-push   r0
 ld     r0, 0x00000002
-push   r0
-ld     r0, 0x00000003
-push   r0
-ld     r0, 0x00000004
-push   r0
-call   DrawRect
+ld     r1, 0x00000004
+div    r0, r1, r0
+ld     r0, [banana+r0]
+st     [VRAM+0x00000004], r0
 ret    
 
 ---------------------------
@@ -139,6 +59,4 @@ ret
 @LABEL DrawRect:spanY
 @DW 0x0000 0x0000
 @LABEL DrawRect:address
-@DW 0x0000 0x0000
-@LABEL main:i
 @DW 0x0000 0x0000
